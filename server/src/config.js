7b993c8e2f7898fc1bdabcd46907349581729586
Fankadore <rmackenzie88@gmail.com>
2018-12-17 23:04:28 +0000

    Added max player level

--- server/src/config.js
+++ server/src/config.js
@@ -12,6 +12,7 @@ config.MAX_MAPS = 10;
 config.MAX_USERS = 100;
 config.MAX_SPRITES = 13;
 config.MAX_EFFECTS = 71;
+config.MAX_LEVEL = 30;
 
 config.MAX_HEALTH_BASE = 200;
 config.MAX_HEALTH_BONUS = 55;
@@ -20,16 +21,5 @@ config.MAX_ENERGY_BONUS = 55;
 
 config.INVENTORY_SIZE = 20;
 config.EQUIPMENT_SIZE = 5;
-
-config.START_MAP = 1;
-config.START_X = 5;
-config.START_Y = 5;
-config.START_NAME = 'New Player';
-config.START_SPRITE = 1;
-config.START_DAMAGE = 2;
-config.START_DEFENCE = 0;
-config.START_HEALTH_MAX = 20;
-config.START_ENERGY_MAX = 10;
-config.START_RANGE = 1;
 
 export default config;
