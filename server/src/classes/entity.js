89ceb704308f6ca3f6c08b321c85e7f70620d127
Fankadore <rmackenzie88@gmail.com>
2018-12-17 23:02:55 +0000

    Changed minimum sprite to 1

--- server/src/classes/entity.js
+++ server/src/classes/entity.js
@@ -3,11 +3,11 @@ import config from '../config.js';
 // An Entity is any object which can appear on the map
 
 export default class Entity {
-	constructor(mapId, x, y, sprite = 0) {
+	constructor(mapId, x, y, sprite = 1) {
 		this.mapId = mapId;
 		this.x = x;
 		this.y = y;
-		if (sprite < 0) sprite = 0;
+		if (sprite < 1) sprite = 1;
 		this.sprite = sprite;
 		this.isVisible = true;
 	}
