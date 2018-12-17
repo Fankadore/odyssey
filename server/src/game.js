c1d66735058a310719e192006cd424c60f243aa5
Fankadore <rmackenzie88@gmail.com>
2018-12-17 23:11:15 +0000

    Update bots, items, effects and texts separately from maps

--- server/src/game.js
+++ server/src/game.js
@@ -1,8 +1,6 @@
-import fs from 'fs';
-
 import db from './db.js';
 import config from './config.js';
-import util from './util.js';
+
 import Map from './classes/map.js';
 import Player from './classes/player.js';
 import Bot from './classes/bot.js';
@@ -13,51 +11,150 @@ import Message from './classes/message.js';
 
 class Game {
 	constructor() {
-		this.playerList = [];
-		this.mapList = [];
+		this.maps = [];
+		this.players = [];
+		this.bots = [];
+		this.items = [];
+		this.effects = [];
+		this.texts = [];
 		this.messageQueue = [];
+		
+		this.playerTemplates = {};
+		this.botTemplates = {};
+		this.itemTemplates = {};
+		this.itemTypes = {};
 
-		// Create Maps
-		this.mapData = JSON.parse(fs.readFileSync('./server/data/maps.json', 'utf8'));
-		for (let id = 0; id < config.MAX_MAPS; id++) {
-			this.mapList[id] = new Map(id, this.mapData[id]);
+		this.experienceToLevel = [];
+		let exp = 10
+		for (let i = 0; i < config.MAX_LEVEL; i++) {
+			exp = (exp + (exp % 2)) * 1.5;
+			this.experienceToLevel[i] = exp;
 		}
+		this.loadMaps();
+		this.loadPlayerTemplates();
+		this.loadBotTemplates();
+		// this.loadItemTemplates();
+	}
+
+	loadMaps() {
+		db.getAllMaps()
+		.then(mapData => {
+			const orderedMapData = [];
+			for (let id = 0; id < mapData.length; id++) {
+				const data = mapData[id];
+				if (data) orderedMapData[data.mapId] = data;
+			}
+
+			for (let id = 0; id < config.MAX_MAPS; id++) {
+				if (orderedMapData[id]) {
+					this.maps[id] = new Map(id, orderedMapData[id]);
+				}
+				else {
+					this.maps[id] = new Map(id);
+				}
+			}
+		})
+		.catch(err => console.log(err));
+	}
+
+	loadPlayerTemplates() {
+		db.getAllPlayerTemplates()
+		.then(templates => {
+			this.playerTemplates = {};
+			templates.forEach(template => {
+				this.playerTemplates[template._id] = template;
+			});
+		}).catch(err => console.log(err));
+	}
+
+	loadBotTemplates() {
+		db.getAllBotTemplates()
+		.then(templates => {
+			this.botTemplates = {};
+			templates.forEach(template => {
+				this.botTemplates[template._id] = template;
+			});
+		}).catch(err => console.log(err));
+	}
+
+	loadItemTemplates() {
+		db.getAllItemTemplates()
+		.then(templates => {
+			this.itemTemplates = {};
+			templates.forEach(template => {
+				this.itemTemplates[template._id] = template;
+			});
+		}).catch(err => console.log(err));
 	}
 
 	update(delta) {
 		let pack = {
 			players: [],
-			maps: [],
-			messages: this.messageQueue
+			bots: [],
+			items: [],
+			effects: [],
+			texts: [],
+			messages: [].concat(this.messageQueue)
 		};
 		this.messageQueue = [];
 
-		this.playerList.forEach((player) => {
-			pack.players[player.id] = player.update(delta);
-		});
-		
-		this.mapList.forEach((map) => {
-			pack.maps[map.id] = map.update(delta);
-		});
+		for (let i = 0; i < this.players.length; i++) {
+			const player = this.players[i];
+			if (player != null) {
+				pack.players[player.gameId] = player.update(delta);
+			}
+		}
+		for (let i = 0; i < this.bots.length; i++) {
+			const bot = this.bots[i];
+			if (bot != null) {
+				pack.bots[bot.gameId] = bot.update(delta);
+			}
+		}
+		for (let i = 0; i < this.items.length; i++) {
+			const item = this.items[i];
+			if (item != null) {
+				pack.items[item.id] = item.update(delta);
+			}
+		}
+		for (let i = 0; i < this.effects.length; i++) {
+			const effect = this.effects[i];
+			if (effect != null) {
+				pack.effects[effect.id] = effect.update(delta);
+			}
+		}
+		for (let i = 0; i < this.texts.length; i++) {
+			const text = this.texts[i];
+			if (text != null) {
+				pack.texts[text.id] = text.update(delta);
+			}
+		}
 
 		return pack;
 	}
 
 	// Players
-	playerLogin(id) {
-		const player = new Player(id);
-		this.playerList[id] = player;
+	playerLogin(socketId, data) {
+		for (let player of this.players) {
+			if (player && player.name === data.name) {
+				console.log("Player is already signed in.");
+				return null;
+			}
+		}
+
+		const player = new Player(socketId, data);
+		db.log(`${socketId} - ${player.name} has logged in.`);
 		this.sendGameInfoGlobal(`${player.name} has logged in.`);
 		return player;
 	}
-	
-	playerLogout(id) {
-		let player = this.playerList[id];
+	playerLogout(playerId) {
+		let player = this.players[playerId];
 		if (player) {
-			db.savePlayerData(player.getPack);
+			const playerData = player.getDBPack()
+			db.log(`${player.socketId} - ${player.name} has logged out.`);
 			this.sendGameInfoGlobal(`${player.name} has logged out.`);
-			delete this.mapList[player.mapId].texts[player.displayNameId];
-			delete this.playerList[id];
+			delete this.texts[player.displayNameId];
+			delete this.players[playerId];
+			db.savePlayer(playerData);
 		}
 	}
 
@@ -89,45 +186,44 @@ class Game {
 		if (x < 0 || x >= config.MAP_COLUMNS) return false;
 		if (y < 0 || y >= config.MAP_ROWS) return false;
 		
-		let map = this.mapList[mapId];
-		
 		// Check for Wall Tiles
-		if (map.tiles.wall[y][x] === true) return false;
+		let map = this.maps[mapId];
+		if (map.isWall[y][x] === true) return false;
 		
 		// Check for Bots
-		let bots = map.bots.filter((bot) => {
-			if (bot.x === x && bot.y === y && !bot.isDead) return true;
+		let bots = this.bots.filter((bot) => {
+			if (bot.mapId === mapId && bot.x === x && bot.y === y && !bot.isDead) return true;
 		});
 		if (bots.length > 0) return false;
 		
 		// Check for Players
-		let players = this.playerList.filter((player) => {
-			if (player.mapId === map.id && player.x === x && player.y === y && !player.isDead) return true;
+		let players = this.players.filter((player) => {
+			if (player.mapId === mapId && player.x === x && player.y === y && !player.isDead) return true;
 		});
 		if (players.length > 0) return false;
 
 		return true;
 	}
 
-	spawnBot(mapId, x, y, botClass) {
-		new Bot({
-			mapId,
-			x,
-			y,
-			botClass
-		});
+	spawnBot(mapId, x, y, templateId) {
+		const direction = 'down';
+		const template = this.botTemplates[templateId];
+		if (template) {
+			new Bot(mapId, x, y, direction, template);
+		}
+		else {
+			console.log("Bot Template does not exist with that Id");
+		}
 	}
 	
-	spawnMapItem(mapId, x, y, itemClass, stack = 0) {
-		new Item({
-			owner: 'map',
-			mapId,
-			id: util.firstEmptyIndex(this.mapList[mapId].items),
-			x,
-			y,
-			itemClass,
-			stack
-		});
+	async spawnMapItem(mapId, x, y, templateId, stack = 0) {
+		let template = await db.getItemTemplate(templateId);
+		if (template) {
+			new Item(mapId, x, y, template, stack);
+		}
+		else {
+			console.log("Item Template does not exist with that Id");
+		}
 	}
 
 	spawnDamageText(mapId, x, y, damage) {
