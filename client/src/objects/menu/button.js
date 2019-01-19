6ae5b3fc41d84ea5b2253d601b7c5797b4033235
Fankadore <rmackenzie88@gmail.com>
2019-01-19 17:25:41 +0000

    Added button menu object

---
+++ client/src/objects/menu/button.js
@@ -0,0 +1,8 @@
+import Phaser from '../../lib/phaser.js';
+
+export default class InputBox extends Phaser.GameObjects.GameObject {
+	constructor(scene, x, y, width, height, message = "") {
+		super(scene);
+		
+	}
+}
