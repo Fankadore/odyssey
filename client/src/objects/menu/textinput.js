94831d4b4fa1798214f0124ce40014c478c9d295
Fankadore <rmackenzie88@gmail.com>
2019-01-19 17:24:59 +0000

    Added ability to hide password entry

--- client/src/objects/menu/textinput.js
+++ client/src/objects/menu/textinput.js
@@ -2,33 +2,33 @@ import Phaser from '../../lib/phaser.js';
 import config from '../../config.js';
 
 export default class InputBox extends Phaser.GameObjects.GameObject {
-	constructor(scene, x, y, width, height) {
+	constructor(scene, x, y, width, height, defaultMessage = "") {
 		super(scene);
+		this.password = false;
+		this.message = defaultMessage;
 		scene.graphics.fillStyle(0xFFFFFF);
 		this.background = new Phaser.Geom.Rectangle(x, y, width, height);
 		scene.graphics.fillRectShape(this.background);
-		this.inputText = scene.add.text(x + 4, y, "Testing Testing", config.FONT_STYLE).setOrigin(0, -0.25);
-		this.focus = false;
-	}
-
-	update() {
-		if (this.focus) {
-			// Update Cursor
-		}
+		this.inputText = scene.add.text(x + 4, y, "", config.FONT_STYLE).setOrigin(0, -0.25);
 	}
 	
 	setFocus(focus) {
-		this.focus = focus;
 		if (focus) this.scene.graphics.lineStyle(2, 0xFF0000);
 		else this.scene.graphics.lineStyle(2, 0x000000);
 		this.scene.graphics.strokeRectShape(this.background);
 	}
 
 	addChar(char) {
-		this.inputText.text += char;
+		this.message += char;
+		let text = this.message;
+		if (this.password) text = text.replace(/./g, '*');
+		this.inputText.setText(text);
 	}
 
 	removeChar() {
-		this.inputText.text.pop();
+		this.message = this.message.slice(0, -1);
+		let text = this.message;
+		if (this.password) text = text.replace(/./g, '*');
+		this.inputText.setText(text);
 	}
 }
