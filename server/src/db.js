1c71013f780b666b03f58be06beb5dc1ce856a20
Fankadore <rmackenzie88@gmail.com>
2019-01-19 14:30:30 +0000

    Added ability to save and retreive Bots and Items from the database

--- server/src/db.js
+++ server/src/db.js
@@ -6,9 +6,11 @@ import util from "./util.js";
 import Account from './models/account.js';
 import Player from './models/player.js';
 import PlayerTemplate from './models/playerTemplate.js';
+import Bot from './models/bot.js';
 import BotTemplate from './models/botTemplate.js';
-import ItemType from './models/itemType.js';
+import Item from './models/item.js';
 import ItemTemplate from './models/itemTemplate.js';
+import ItemType from './models/itemType.js';
 import Map from './models/map.js';
 
 const fsp = fs.promises;
@@ -24,10 +26,10 @@ class Database {
 		//TODO save everything
 		// const maps = save-all-maps
 		let players = this.saveOnlinePlayers(data.players);
-		// const bots = save-all-bots
-		// const items = save-all-items
+		const bots = this.saveAllBots(data.bots);
+		let items = this.saveAllItems(data.items);
 		let logSaved = this.saveLog();
-		Promise.all([players, logSaved])
+		Promise.all([players, bots, items, logSaved])
 		.then(() => this.log("Game saved to disk."));
 	}
 	
@@ -64,6 +66,9 @@ class Database {
 		}
 	}
 
+	generateId() {
+		return new mongoose.Types.ObjectId;
+	}
 	async hashPassword(password) {
 		return await new Promise((resolve, reject) => {
 			bcrypt.hash(password, 10, (err, hash) => {
@@ -81,7 +86,7 @@ class Database {
 		});
 	}
 	async authAccount(username, password) {
-		const account = await Account.findOne({username: username}).exec();
+		let account = await Account.findOne({username: username}).exec();
 		if (!account) return false;
 		
 		return this.comparePassword(password, account.password);
@@ -198,36 +203,35 @@ class Database {
 		for (let i = 0; i < players.length; i++) {
 			const player = players[i];
 			if (!player) continue;
-			this.savePlayer(player.getDBPack());
+			this.savePlayer(player);
 		}
 	}
 
-	async addBot(templateName, mapId, x, y) {
-		let templateId = await this.getPlayerTemplateId(templateName);
-		if (!templateId) {
-			console.log("Bot Template does not exist with that name.");
-			return false;
-		}
-		let bot = await this.getBot(name);
-		if (bot) {
-			console.log("Bot already exists with that name.");
+	async addBot(data) {
+		let template = await this.getBotTemplate(data.templateId);
+		if (!template) {
+			console.log("Bot Template does not exist with that id.");
 			return false;
 		}
 
-		bot = new Bot({
-			_id : new mongoose.Types.ObjectId,
-			template: templateId,
-			mapId,
-			x,
-			y
+		let _id = data.botId;
+		if (!_id) _id = new mongoose.Types.ObjectId;
+
+		const bot = new Bot({
+			_id,
+			template: data.templateId,
+			mapId: data.mapId,
+			x: data.x,
+			y: data.y,
+			direction: data.direction
 		});
 
 		return await bot.save()
 		.then(result => true)
 		.catch(err => console.log(err));
 	}
-	async getBot(name) {
-		return await Bot.findOne({name: name})
+	async getBot(botId) {
+		return await Bot.findOne({_id: botId})
 		.select('_id name sprite template level experience mapId x y direction')
 		.populate('template')
 		.exec()
@@ -235,10 +239,46 @@ class Database {
 		.catch(err => console.log(err));
 	}
 	async saveBot(data) {
-		return await Bot.updateOne({name: data.name}, {$set: data})
+		return await Bot.updateOne({_id: data.botId}, {$set: data})
 		.exec()
 		.then(result => true)
 		.catch(err => console.log(err));
+	}
+	async getAllBots() {
+		return await Bot.find({})
+		.select('_id template mapId x y direction')
+		.populate('template')
+		.exec()
+		.then(bots => bots)
+		.catch(err => console.log(err));
+	}
+	async saveAllBots(currentBots) {
+		if (!currentBots) return;
+
+		let savedBots = await this.getAllBots();
+		const newBots = currentBots.filter(bot => !savedBots.find(savedBot => savedBot._id === bot.botId));
+		const existingBots = currentBots.filter(bot => savedBots.find(savedBot => savedBot._id === bot.botId));
+		const deleteBots = savedBots.filter(bot => !existingBots.find(existingBot => existingBot.botId === bot._id));
+		const updateBots = existingBots.filter(bot => !deleteBots.includes(bot));
+
+		// Add new Bots
+		for (let i = 0; i < newBots.length; i++) {
+			this.addBot(newBots[i]);
+		}
+
+		// Delete removed Bots
+		for (let i = 0; i < deleteBots.length; i++) {
+			Bot.deleteOne({_id: deleteBots[i]._id}, err => {
+				if (err) console.log(err);
+			});
+		}
+
+		// Update the rest
+		for (let i = 0; i < updateBots.length; i++) {
+			const bot = updateBots[i];
+			if (!bot) continue;
+			this.saveBot(bot);
+		}
 	}
 
 	async getMap(mapId) {
@@ -334,7 +374,7 @@ class Database {
 	}
 	async getBotTemplate(templateId) {
 		return await BotTemplate.findById(templateId)
-		.select('name sprite damageBase defenceBase healthMaxBase energyMaxBase rangeBase hostile')
+		.select('_id name sprite damageBase defenceBase healthMaxBase energyMaxBase rangeBase hostile')
 		.exec()
 		.then(template => template)
 		.catch(err => console.log(err));
@@ -412,6 +452,88 @@ class Database {
 		.exec()
 		.then(templates => templates)
 		.catch(err => console.log(err));
+	}
+
+	async addItem(data) {
+		let template = await this.getItemTemplate(data.templateId);
+		if (!template) {
+			console.log("Item Template does not exist with that id.");
+			return false;
+		}
+		let _id = data.itemId;
+		if (!_id) _id = new mongoose.Types.ObjectId;
+
+		const item = new Item({
+			_id,
+			template: data.templateId,
+			stack: data.stack,
+			playerId: data.playerId,
+			botId: data.botId,
+			slot: data.slot,
+			mapId: data.mapId,
+			x: data.x,
+			y: data.y,
+			createdBy: data.createdBy,
+			createdDate: data.createdDate
+		});
+
+		return await item.save()
+		.then(result => true)
+		.catch(err => console.log(err));
+	}
+	async getItem(itemId) {
+		return await Item.findOne({_id: itemId})
+		.select('_id template stack playerId botId slot mapId x y createdDate createdBy')
+		.exec()
+		.then(item => item)
+		.catch(err => console.log(err));
+	}
+	async saveItem(data) {
+		return await Item.updateOne({_id: data.itemId}, {$set: data}, {upsert: true})
+		.exec()
+		.then(result => true)
+		.catch(err => console.log(err));
+	}
+	async getAllItems() {
+		try {
+			return await Item.find({})
+			.select('_id template stack playerId botId slot mapId x y createdDate createdBy')
+			.populate('template')
+			.exec()
+			.then(items => items)
+			.catch(err => console.log(err));
+		}
+		catch(err) {
+			console.log(err);
+		}
+	}
+	async saveAllItems(currentItems) {
+		if (!currentItems) return;
+
+		let savedItems = await this.getAllItems();
+		const newItems = currentItems.filter(item => !savedItems.find(savedItem => savedItem._id === item.itemId));
+		const existingItems = currentItems.filter(item => savedItems.find(savedItem => savedItem._id === item.itemId));
+		const deleteItems = savedItems.filter(item => !existingItems.find(existingItem => existingItem.itemId === item._id));
+		const updateItems = existingItems.filter(item => !deleteItems.includes(item));
+		
+		// Add new Items
+		for (let i = 0; i < newItems.length; i++) {
+			this.addItem(newItems[i]);
+		}
+
+		// Delete removed Items
+		for (let i = 0; i < deleteItems.length; i++) {
+			Item.deleteOne({_id: deleteItems[i]._id}, err => {
+				if (err) console.log(err);
+			});
+		}
+
+		// Update the rest
+		for (let i = 0; i < updateItems.length; i++) {
+			const item = updateItems[i];
+			if (!item) continue;
+			this.saveItem(item);
+		}
 	}
 }
 
