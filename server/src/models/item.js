19abeba0cd35e53685042186a951e3379b62679b
Fankadore <rmackenzie88@gmail.com>
2019-01-19 14:26:06 +0000

    Changed playerId and botId from Number to ObjectId

--- server/src/models/item.js
+++ server/src/models/item.js
@@ -4,14 +4,14 @@ const itemSchema = new mongoose.Schema({
   _id: mongoose.SchemaTypes.ObjectId,
   template: {type: mongoose.SchemaTypes.ObjectId, ref: 'ItemTemplate', required: true},
   stack: {type: Number, default: 0},
-  playerId: {type: Number, default: null},
-  botId: {type: Number, default: null},
+  playerId: {type: mongoose.SchemaTypes.ObjectId, default: null},
+  botId: {type: mongoose.SchemaTypes.ObjectId, default: null},
   slot: {type: Number, default: null},
   mapId: {type: Number, default: null},
   x: {type: Number, default: null},
   y: {type: Number, default: null},
-  createdDate: {type: Date},
-  createdBy: {type: String}
+  createdBy: {type: String},
+  createdDate: {type: Date}
 });
 
 export default mongoose.model('Item', itemSchema);
