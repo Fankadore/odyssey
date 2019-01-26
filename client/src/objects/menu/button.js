97d71f6704c8c0832a098fc85a6316f8b23e42c1
Fankadore <rmackenzie88@gmail.com>
2019-01-26 13:12:09 +0000

    Added UI containers

--- client/src/objects/menu/button.js
+++
@@ -1,8 +0,0 @@
-import Phaser from '../../lib/phaser.js';
-
-export default class InputBox extends Phaser.GameObjects.GameObject {
-	constructor(scene, x, y, width, height, message = "") {
-		super(scene);
-		
-	}
-}
