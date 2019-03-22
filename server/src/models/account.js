6542c4606460d50760bc5879cb75a6663e0ef8d8
Fankadore <rmackenzie88@gmail.com>
2019-03-22 21:41:55 +0000

    Added usernameLowerCase to account schema

--- server/src/models/account.js
+++ server/src/models/account.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const accountSchema = new mongoose.Schema({
   _id: mongoose.SchemaTypes.ObjectId,
   username: {type: String, required: true, unique: true},
+  usernameLowerCase: {type: String, required: true, unique: true},
   password: {type: String, required: true},
   email: {type: String, match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/},
   verified: {type: Boolean, default: false},
