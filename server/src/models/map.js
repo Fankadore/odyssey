2955e0e1317b38ccd5725fbc8055d584d62fc03c
Fankadore <fankadore@protonmail.com>
2019-08-20 17:40:15 +0100

    Added default settings for maps

--- server/src/models/map.js
+++ server/src/models/map.js
@@ -1,18 +1,22 @@
 import mongoose from 'mongoose';
 
+const defaultTiles = new Array(6).fill(new Array(12).fill(new Array(12).fill(0)));
+const falseLayer = new Array(12).fill(new Array(12).fill(false));
+const nullLayer = new Array(12).fill(new Array(12).fill(null));
+
 const mapSchema = new mongoose.Schema({
 	_id: mongoose.SchemaTypes.ObjectId,
-	mapId: Number,
+	mapId: {type: Number, required: true},
 	name: {type: String, required: true},
 	dropChance: {type: Number, default: 100},
 	dropAmountEQ: {type: Number, default: 1},
-	tiles: {type: [[[Number]]], default: [[[]]]},
-	isWall: {type: [[Boolean]], default: false},
-	isHostile: {type: [[Boolean]], default: false},
-	damage: {type: [[Number]], default: null},
-	warpMap: {type: [[Number]], default: null},
-	warpX: {type: [[Number]], default: null},
-	warpY: {type: [[Number]], default: null}
+	tiles: {type: [[[Number]]], default: defaultTiles},
+	isWall: {type: [[Boolean]], default: falseLayer},
+	isHostile: {type: [[Boolean]], default: falseLayer},
+	damage: {type: [[Number]], default: nullLayer},
+	warpMap: {type: [[Number]], default: nullLayer},
+	warpX: {type: [[Number]], default: nullLayer},
+	warpY: {type: [[Number]], default: nullLayer},
 });
 
 export default mongoose.model('Map', mapSchema);
